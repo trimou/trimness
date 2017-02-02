9e82f0d3dc328e36cfbb60e04af7e20ca3868a85
Martin Kouba <mkouba@redhat.com>
2017-02-02 16:46:06 +0100

    Upgrade Weld dependencies and make use of weld-junit4

--- basis/src/test/java/org/trimou/basis/FileSystemTemplateRepositoryTest.java
+++ basis/src/test/java/org/trimou/basis/FileSystemTemplateRepositoryTest.java
@@ -12,8 +12,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.UUID;
 
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
+import org.jboss.weld.junit4.WeldInitiator;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -22,58 +22,40 @@ import org.junit.Test;
  */
 public class FileSystemTemplateRepositoryTest {
 
+    @Rule
+    public WeldInitiator weld = WeldInitiator.of(FileSystemTemplateRepository.class, MockVertxProducer.class, DummyBasisConfiguration.class);
+
     @Test
     public void testBasicOperations() {
-        try (WeldContainer container = new Weld().disableDiscovery()
-                .beanClasses(FileSystemTemplateRepository.class,
-                        MockVertxProducer.class, DummyBasisConfiguration.class)
-                .initialize()) {
-            DummyBasisConfiguration configuration = container
-                    .select(DummyBasisConfiguration.class).get();
-            configuration.put(FS_TEMPLATE_REPO_DIR,
-                    "src/test/resources/templates");
-            configuration.put(FS_TEMPLATE_REPO_MATCH, ".*\\.html");
+        DummyBasisConfiguration configuration = weld.select(DummyBasisConfiguration.class).get();
+        configuration.put(FS_TEMPLATE_REPO_DIR, "src/test/resources/templates");
+        configuration.put(FS_TEMPLATE_REPO_MATCH, ".*\\.html");
 
-            FileSystemTemplateRepository repository = container
-                    .select(FileSystemTemplateRepository.class).get();
-            assertEquals(1, repository.getAll().size());
-            assertNull(repository.get("hello.txt"));
-            Template helloTemplate = repository.get("hello.html");
-            assertNotNull(helloTemplate);
-            assertEquals("hello.html", helloTemplate.getId());
-            assertEquals("<html><body>Hello {{name}}!</body></html>",
-                    helloTemplate.getContent());
-        }
+        FileSystemTemplateRepository repository = weld.select(FileSystemTemplateRepository.class).get();
+        assertEquals(1, repository.getAll().size());
+        assertNull(repository.get("hello.txt"));
+        Template helloTemplate = repository.get("hello.html");
+        assertNotNull(helloTemplate);
+        assertEquals("hello.html", helloTemplate.getId());
+        assertEquals("<html><body>Hello {{name}}!</body></html>", helloTemplate.getContent());
     }
 
     @Test
     public void testScanInterval() throws IOException, InterruptedException {
-        try (WeldContainer container = new Weld().disableDiscovery()
-                .beanClasses(FileSystemTemplateRepository.class,
-                        MockVertxProducer.class, DummyBasisConfiguration.class)
-                .initialize()) {
+        DummyBasisConfiguration configuration = weld.select(DummyBasisConfiguration.class).get();
+        configuration.put(FS_TEMPLATE_REPO_DIR, "src/test/resources/templates");
+        configuration.put(FS_TEMPLATE_REPO_SCAN_INTERVAL, 100l);
 
-            DummyBasisConfiguration configuration = container
-                    .select(DummyBasisConfiguration.class).get();
-            configuration.put(FS_TEMPLATE_REPO_DIR,
-                    "src/test/resources/templates");
-            configuration.put(FS_TEMPLATE_REPO_SCAN_INTERVAL, 100l);
+        FileSystemTemplateRepository repository = weld.select(FileSystemTemplateRepository.class).get();
 
-            FileSystemTemplateRepository repository = container
-                    .select(FileSystemTemplateRepository.class).get();
-
-            String id = UUID.randomUUID().toString();
-            File tmpFile = new File(
-                    new File(
-                            configuration.getStringValue(FS_TEMPLATE_REPO_DIR)),
-                    "temp_hello.txt");
-            Files.write(tmpFile.toPath(), id.getBytes());
-            Thread.sleep(200);
-            Template helloTemp = repository.get("temp_hello.txt");
-            assertNotNull(helloTemp);
-            assertEquals(id, helloTemp.getContent());
-            tmpFile.delete();
-        }
+        String id = UUID.randomUUID().toString();
+        File tmpFile = new File(new File(configuration.getStringValue(FS_TEMPLATE_REPO_DIR)), "temp_hello.txt");
+        Files.write(tmpFile.toPath(), id.getBytes());
+        Thread.sleep(200);
+        Template helloTemp = repository.get("temp_hello.txt");
+        assertNotNull(helloTemp);
+        assertEquals(id, helloTemp.getContent());
+        tmpFile.delete();
     }
 
 }
--- basis/src/test/java/org/trimou/basis/GlobalJsonDataItemProviderTest.java
+++ basis/src/test/java/org/trimou/basis/GlobalJsonDataItemProviderTest.java
@@ -7,8 +7,8 @@ import static org.trimou.basis.BasisConfigurationKey.GLOBAL_JSON_DATA_FILE;
 
 import java.util.List;
 
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
+import org.jboss.weld.junit4.WeldInitiator;
+import org.junit.Rule;
 import org.junit.Test;
 import org.trimou.basis.DataItemProvider.DataItem;
 
@@ -21,41 +21,35 @@ import com.google.gson.JsonElement;
  */
 public class GlobalJsonDataItemProviderTest {
 
+    @Rule
+    public WeldInitiator weld = WeldInitiator.of(GlobalJsonDataItemProvider.class, DummyBasisConfiguration.class);
+
     @Test
     public void testBasicOperations() {
-        try (WeldContainer container = new Weld().disableDiscovery()
-                .beanClasses(GlobalJsonDataItemProvider.class,
-                        DummyBasisConfiguration.class)
-                .initialize()) {
+        DummyBasisConfiguration configuration = weld.select(DummyBasisConfiguration.class).get();
+        configuration.put(GLOBAL_JSON_DATA_FILE, "src/test/resources/global-data.json");
 
-            DummyBasisConfiguration configuration = container
-                    .select(DummyBasisConfiguration.class).get();
-            configuration.put(GLOBAL_JSON_DATA_FILE,
-                    "src/test/resources/global-data.json");
+        GlobalJsonDataItemProvider provider = weld.select(GlobalJsonDataItemProvider.class).get();
 
-            GlobalJsonDataItemProvider provider = container
-                    .select(GlobalJsonDataItemProvider.class).get();
+        List<DataItem> data = provider.getData("whatever");
 
-            List<DataItem> data = provider.getData("whatever");
+        assertNotNull(data);
+        assertEquals(2, data.size());
+        assertEquals("foo", data.get(0).getName());
+        assertEquals("array", data.get(1).getName());
 
-            assertNotNull(data);
-            assertEquals(2, data.size());
-            assertEquals("foo", data.get(0).getName());
-            assertEquals("array", data.get(1).getName());
+        Object fooData = data.get(0).getValue();
 
-            Object fooData = data.get(0).getValue();
+        assertTrue(fooData instanceof JsonElement);
+        JsonElement fooElement = (JsonElement) fooData;
+        assertEquals("bar", fooElement.getAsString());
 
-            assertTrue(fooData instanceof JsonElement);
-            JsonElement fooElement = (JsonElement) fooData;
-            assertEquals("bar", fooElement.getAsString());
+        Object arrayData = data.get(1).getValue();
 
-            Object arrayData = data.get(1).getValue();
-
-            assertTrue(arrayData instanceof JsonArray);
-            JsonArray arrayElement = (JsonArray) arrayData;
-            assertEquals(3, arrayElement.size());
-            assertEquals(1, arrayElement.get(0).getAsInt());
-        }
+        assertTrue(arrayData instanceof JsonArray);
+        JsonArray arrayElement = (JsonArray) arrayData;
+        assertEquals(3, arrayElement.size());
+        assertEquals(1, arrayElement.get(0).getAsInt());
     }
 
 }
